1e7f4e6f340332f7f179db592484f7846269b6a6
Canon <Canon@CanondeMacBook-Prox.local>
2017-12-21 19:41:41 +0800

    123

--- src/python/diff.py
+++ src/python/diff.py
@@ -7,12 +7,22 @@ DEFAULT_SYS_ARGV_LEN = 3
 HEP_SYS_ARGV_LEN = 2
 
 class Diffiner(object):
+    
+    FIRST_SLICE = 1
 
-    def __init__(self,ol,nl):
+    def __init__(self, ol, nl):
         self.old_list = ol
         self.new_file = nl
-    
+
+    def get_diff_array(self, ol, nl):
+        for index_l, ol_line in enumerate(ol):
+            for index_n, nl_line in enumerate(nl):
+                if ol[self.FIRST_INDEX] == nl_line:
+                    pair_count, pair_array = self.get_diff_array(ol[self.FIRST_SLICE:], nl[index_n + self.FIRST_SLICE])
+
+            
     def get_diff(self):
+        self.get_diff_array(self.old_list,self.new_file)
         return 1
 
 if __name__ == '__main__':
